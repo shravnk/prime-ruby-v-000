12b6249ca11a1843092f4ba5c69e1d0346f12fbd
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-30 22:25:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,34 +1,10 @@
-def prime?(num)
-  case num
-    when num == 2
-       return true
-    when num == 3
-        return true
-    when num % 2 == 0
-      return false
-    when num % 3 == 0
-      return false
-    else
-      i = 5
-      w = 2
+def prime?
 
-      while i * i <= num
-       if n % i == 0
-           return false
-       end
-       i += w
-       w = 6 - w
-     end
+  primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]
 
-  end
+  primes.each{|prime| puts "prime"}
+
 end
 
-# def test
-#   if prime?(5)
-#     puts "True"
-#   else
-#     puts "False"
-#   end
-# end
-#
-# test
+
+prime?
