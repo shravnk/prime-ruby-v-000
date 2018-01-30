4a29014e94f30a4b5cbc1f7614ff9c8769ce2f34
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-30 21:42:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(num)
+  case num
+  when num == 2
+     return true
+  when num == 3
+      return true
+  when num % 2 == 0
+    return false
+  when num % 3 == 0
+    return false
+  else
+    i = 5
+    w = 2
+    
+    while i * i <= num
+     if n % i == 0
+         return false
+     end
+     i += w
+     w = 6 - w  
+   end
+   return true
+end
